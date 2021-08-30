456ebdead3faa10859f0ff3182f84a42d54755d1
Irechukwu Confidence <irechukwuconfidence@gmail.com>
2021-08-30 06:24:01 +0100

    .

---
+++ js/algo.js
@@ -0,0 +1,27 @@
+
+// const customer_data = {
+// 'Ben10':[22, 30, 11, 17, 15, 52, 27, 12],
+// 'Sameer':[5, 17, 30, 33, 40, 22, 26, 10, 11, 45],
+// 'Zeeshan':[22, 30, 11, 5, 17, 30, 6, 57]
+// }
+
+// for(i = 5; i < customer_data.length; i++){
+
+// }
+// if(customer_data >= 20 && customer_data.length >= i){
+//    console.log(customer_data[i])
+// }
+// console.log(customer_data)
+
+
+
+let people = ["Greg", "Mary", "Devon", "James"];
+for(let i = 0; i<people.length; i++){
+   //(1) Using a loop, iterate through this array and console.log all of the people.
+  console.log(people[i]); 
+  
+// people.unshift();
+ 
+ // 4. write the command to add " Matt to the front of the arry"
+//people.unshift("Matt")
+}
